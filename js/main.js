4e6691c0e1b29e57aaa18a0ebe4d942c81aa25ba
Brayan Lozada <brayanlozada1102@gmail.com>
2026-04-21 11:02:44 -0500

    add button

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function showmenu() {
+    var btn = document.getElementById("nav-button");
+    var menu = document.getElementsByClassName("nav-menu");
+    if (menu[0].classList.contains("active")) {
+        btn.innerText = "≡"
+    } else {
+        btn.innerText = "X"
+    }
+    menu[0].classList.toggle("active")
+}
